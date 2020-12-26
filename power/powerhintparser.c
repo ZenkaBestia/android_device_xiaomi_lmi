5ad5274259a8dd2ddf6e08c0313c2562b7a2996a
TH779 <tongsj2333@gmail.com>
2020-12-26 10:41:44 +0800

    lmi: power: Merge changes from LA.UM.9.12.r1-09500-SMxx50.0
    Signed-off-by: TH779 <tongsj2333@gmail.com>

--- power/powerhintparser.c
+++ power/powerhintparser.c
@@ -26,8 +26,6 @@
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  */
-#define LOG_TAG "QTI PowerHAL"
-
 #include <cutils/log.h>
 #include <fcntl.h>
 #include <string.h>
@@ -36,6 +34,7 @@
 #include <libxml/tree.h>
 #include <unistd.h>
 #include "powerhintparser.h"
+#define LOG_TAG "QTI PowerHAL"
 
 int parsePowerhintXML() {
 
